6ee57d86ba3299e7bbb9bd9de7393a9dd0dd10ae
Martin Liu <liumartin@google.com>
2022-04-20 16:47:57 +0800

    ANDROID: cma: allow to use CMA in swap-in path
    Now, we allow to use CMA pages for certain user space
    allocations. One of them is anonymous page fault case.
    To align the use case, we should also allow to use CMA
    pages in swap-in cases. This could help mitigate OOM
    on swap-in cases showing plenty of free CMA left.
    
    logd.klogd invoked oom-killer: gfp_mask=0x1100cca(GFP_HIGHUSER_MOVABLE), order=0, oom_score_adj=-1000
    CPU: 0 PID: 433 Comm: logd.klogd Tainted: G        W  OE     5.10.100-android13-0 #1
    
    Call trace:
     dump_backtrace.cfi_jt+0x0/0x8
     show_stack+0x1c/0x2c
     dump_stack_lvl+0xc0/0x13c
     dump_header+0x54/0x238
     oom_kill_process+0xb0/0x158
     out_of_memory+0x17c/0x328
     __alloc_pages_slowpath+0x5c4/0x8d0
     __alloc_pages_nodemask+0x1bc/0x2e0
     __read_swap_cache_async+0xdc/0x370
     swap_vma_readahead+0x3b4/0x488
     swapin_readahead+0x3c/0x54
     do_swap_page+0x1e0/0xaa0
     handle_pte_fault+0x128/0x1e0
     handle_mm_fault+0x308/0x590
     do_page_fault+0x33c/0x478
     do_translation_fault+0x58/0x11c
     do_mem_abort+0x68/0x144
     el0_da+0x24/0x34
     el0_sync_handler+0xc4/0xec
     el0_sync+0x1c0/0x200
    Mem-Info:
    active_anon:0 inactive_anon:3222 isolated_anon:62
     active_file:232 inactive_file:428 isolated_file:0
     unevictable:37232 dirty:3 writeback:40
     slab_reclaimable:19943 slab_unreclaimable:281193
     mapped:37126 shmem:2815 pagetables:8981 bounce:0
     free:126007 free_pcp:223 free_cma:123062
    Node 0 active_anon:16kB inactive_anon:13160kB active_file:292kB inactive_file:2000kB unevictable:148928kB isolated(anon):0kB isolated(file):0kB mapped:148308kB dirty:12kB writeback:164kB shmem:11260kB shmem_thp: 0kB shmem_pmdmapped: 0kB anon_thp: 0kB writeback_tmp:0kB kernel_stack:20528kB shadow_call_stack:5200kB all_unreclaimable? no
    DMA32 free:14128kB min:7572kB low:22636kB high:37700kB reserved_highatomic:4096KB active_anon:0kB inactive_anon:0kB active_file:0kB inactive_file:0kB unevictable:0kB writepending:0kB present:1913856kB managed:1553276kB mlocked:0kB pagetables:1292kB bounce:0kB free_pcp:0kB local_pcp:0kB free_cma:2520kB
    lowmem_reserve[]: 0 0 0
    Normal free:489888kB min:19808kB low:59220kB high:98632kB reserved_highatomic:36864KB active_anon:20kB inactive_anon:12168kB active_file:0kB inactive_file:1640kB unevictable:148928kB writepending:180kB present:4194304kB managed:4063392kB mlocked:148928kB pagetables:34632kB bounce:0kB free_pcp:1928kB local_pcp:0kB free_cma:489752kB
    lowmem_reserve[]: 0 0 0
    DMA32: 166*4kB (UME) 163*8kB (UMECH) 592*16kB (UMCH) 5*32kB (UC) 2*64kB (C) 2*128kB (C) 2*256kB (C) 1*512kB (C) 1*1024kB (C) 0*2048kB 0*4096kB = 14032kB
    Normal: 969*4kB (C) 77*8kB (C) 40*16kB (C) 17*32kB (C) 5*64kB (C) 1*128kB (C) 2*256kB (C) 1*512kB (C) 0*1024kB 0*2048kB 118*4096kB (C) = 490476kB
    40220 total pagecache pages
    30 pages in swap cache
    Swap cache stats: add 2634625, delete 2635304, find 160621/2963954
    Free swap  = 1473788kB
    Total swap = 2097148kB
    
    Bug: 229822798
    Signed-off-by: Martin Liu <liumartin@google.com>
    Change-Id: Ia0bb6f72e52f77f26062e1769bfd92e831f07cab
    Signed-off-by: Richard Chang <richardycc@google.com>
    (cherry picked from commit 3e591c63b13772a1de0ffed995b482166d27ed71)

--- mm/memory.c
+++ mm/memory.c
@@ -3565,8 +3565,8 @@ vm_fault_t do_swap_page(struct vm_fault *vmf)
 		if (data_race(si->flags & SWP_SYNCHRONOUS_IO) &&
 		    __swap_count(entry) == 1) {
 			/* skip swapcache */
-			page = alloc_page_vma(GFP_HIGHUSER_MOVABLE, vma,
-							vmf->address);
+			page = alloc_page_vma(GFP_HIGHUSER_MOVABLE | __GFP_CMA,
+					      vma, vmf->address);
 			if (page) {
 				__SetPageLocked(page);
 				__SetPageSwapBacked(page);
@@ -3590,7 +3590,8 @@ vm_fault_t do_swap_page(struct vm_fault *vmf)
 				set_page_private(page, 0);
 			}
 		} else {
-			page = swapin_readahead(entry, GFP_HIGHUSER_MOVABLE,
+			page = swapin_readahead(entry,
+						GFP_HIGHUSER_MOVABLE | __GFP_CMA,
 						vmf);
 			swapcache = page;
 		}
