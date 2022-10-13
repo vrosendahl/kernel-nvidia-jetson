5fd6e0642cd4d4dcd41a7c786d71cfa1fdf26bf8
Vidya Sagar <vidyas@nvidia.com>
2022-10-14 00:08:47 +0530

    UBUNTU: SAUCE: PCI: tegra194: Allow system suspend when the Endpoint link is not up
    BugLink: https://bugs.launchpad.net/bugs/2001557
    
    Only a Root port initiates the L2 sequence. PCIe link is kept in L2 state
    during suspend. If Endpoint mode is enabled and the link is up, the
    software cannot proceed with suspend. However, when the PCIe Endpoint
    driver is probed, but the PCIe link is not up, Tegra can go into suspend
    state. So, allow system to suspend in this case.
    
    Signed-off-by: Vidya Sagar <vidyas@nvidia.com>
    Signed-off-by: Kartik <kkartik@nvidia.com>
    Acked-by: Brad Figg <bfigg@nvidia.com>
    Acked-by: Jacob Martin <jacob.martin@canonical.com>
    Acked-by: Ian May <ian.may@canonical.com>
    Signed-off-by: Brad Figg <bfigg@nvidia.com>

--- drivers/pci/controller/dwc/pcie-tegra194.c
+++ drivers/pci/controller/dwc/pcie-tegra194.c
@@ -2367,8 +2367,14 @@ static int tegra_pcie_dw_suspend_late(struct device *dev)
 	u32 val;
 
 	if (pcie->of_data->mode == DW_PCIE_EP_TYPE) {
-		dev_err(dev, "Failed to Suspend as Tegra PCIe is in EP mode\n");
-		return -EPERM;
+		disable_irq(pcie->pex_rst_irq);
+
+		if (pcie->ep_state == EP_STATE_ENABLED) {
+			dev_err(dev, "Tegra PCIe is in EP mode, suspend not allowed");
+			return -EPERM;
+		} else {
+			return 0;
+		}
 	}
 
 	if (!pcie->link_state)
@@ -2390,6 +2396,9 @@ static int tegra_pcie_dw_suspend_noirq(struct device *dev)
 {
 	struct tegra_pcie_dw *pcie = dev_get_drvdata(dev);
 
+	if (pcie->of_data->mode == DW_PCIE_EP_TYPE)
+		return 0;
+
 	if (!pcie->link_state)
 		return 0;
 
@@ -2404,6 +2413,9 @@ static int tegra_pcie_dw_resume_noirq(struct device *dev)
 {
 	struct tegra_pcie_dw *pcie = dev_get_drvdata(dev);
 	int ret;
+
+	if (pcie->of_data->mode == DW_PCIE_EP_TYPE)
+		return 0;
 
 	if (!pcie->link_state)
 		return 0;
@@ -2437,8 +2449,8 @@ static int tegra_pcie_dw_resume_early(struct device *dev)
 	u32 val;
 
 	if (pcie->of_data->mode == DW_PCIE_EP_TYPE) {
-		dev_err(dev, "Suspend is not supported in EP mode");
-		return -ENOTSUPP;
+		enable_irq(pcie->pex_rst_irq);
+		return 0;
 	}
 
 	if (!pcie->link_state)
