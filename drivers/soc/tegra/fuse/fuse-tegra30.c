b62918d72db884c26f60ef3878eb11e15fd85f10
Kartik <kkartik@nvidia.com>
2024-02-07 05:45:34 +0000

    [UPSTREAM PENDING] soc/tegra: fuse: Update Tegra234 nvmem keepout list
    nv_fuse_read.sh utility allows reading following fuse from userspace
    via Fuse nvmem interface:
    
    	* odmid
    	* odminfo
    	* boot_security_info
    	* public_key_hash
    	* reserved_odm0
    	* reserved_odm1
    	* reserved_odm2
    	* reserved_odm3
    	* reserved_odm4
    	* reserved_odm5
    	* reserved_odm6
    	* reserved_odm7
    	* odm_lock
    	* pk_h1
    	* pk_h2
    	* revoke_pk_h0
    	* revoke_pk_h1
    	* security_mode
    	* system_fw_field_ratchet0
    	* system_fw_field_ratchet1
    	* system_fw_field_ratchet2
    	* system_fw_field_ratchet3
    	* optin_enable
    
    Update tegra234_fuse_keepouts list to allow reading these fuse from
    nvmem sysfs interface.
    
    Bug 4208456
    Bug 4408388
    
    Signed-off-by: Kartik <kkartik@nvidia.com>
    Change-Id: I08fc9ff37e5cc17f445f31f357b6e1ac21a223c9
    Reviewed-on: https://git-master.nvidia.com/r/c/3rdparty/canonical/linux-jammy/+/3072038
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>
    (cherry picked from commit 87ef658dc9d20c)
    Reviewed-on: https://git-master.nvidia.com/r/c/3rdparty/canonical/linux-jammy/+/3100380
    Reviewed-on: https://git-master.nvidia.com/r/c/3rdparty/canonical/linux-jammy/+/3107227
    Reviewed-by: svcacv <svcacv@nvidia.com>
    GVS: Gerrit_Virtual_Submit <buildbot_gerritrpt@nvidia.com>

--- drivers/soc/tegra/fuse/fuse-tegra30.c
+++ drivers/soc/tegra/fuse/fuse-tegra30.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2013-2014, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2013-2024, NVIDIA CORPORATION.  All rights reserved.
  */
 
 #include <linux/device.h>
@@ -633,15 +633,19 @@ static const struct nvmem_cell_lookup tegra234_fuse_lookups[] = {
 };
 
 static const struct nvmem_keepout tegra234_fuse_keepouts[] = {
-	{ .start = 0x01c, .end = 0x0c8 },
-	{ .start = 0x12c, .end = 0x184 },
+	{ .start = 0x1c, .end = 0x64 },
+	{ .start = 0x84, .end = 0xc8 },
+	{ .start = 0x12c, .end = 0x164 },
+	{ .start = 0x16c, .end = 0x184 },
 	{ .start = 0x190, .end = 0x198 },
 	{ .start = 0x1a0, .end = 0x204 },
-	{ .start = 0x21c, .end = 0x250 },
-	{ .start = 0x25c, .end = 0x2f0 },
+	{ .start = 0x21c, .end = 0x2f0 },
 	{ .start = 0x310, .end = 0x3d8 },
-	{ .start = 0x400, .end = 0x4f0 },
-	{ .start = 0x4f8, .end = 0x7e8 },
+	{ .start = 0x400, .end = 0x420 },
+	{ .start = 0x444, .end = 0x490 },
+	{ .start = 0x4bc, .end = 0x4f0 },
+	{ .start = 0x4f8, .end = 0x54c },
+	{ .start = 0x57c, .end = 0x7e8 },
 	{ .start = 0x8d0, .end = 0x8d8 },
 	{ .start = 0xacc, .end = 0xf00 }
 };
