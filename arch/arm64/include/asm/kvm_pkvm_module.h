df529dcb840002fc7f59382e9e9596a585fdf247
Mostafa Saleh <smostafa@google.com>
2024-02-25 21:40:08 +0000

    ANDROID: KVM: arm64: iommu: Add pgtable allocation functions
    Soon we would support idmapped domains which uses a separate pool for
    memory allocation.
    
    Add new functions alloc/reclaim functions that use the idmap allocator,
    and a new property to arm_lpae_io_pgtable to distinguish these domains.
    
    It's worth noting that these function shouldn't fail and hence has no
    request arg.
    
    Bug: 277989609
    Bug: 278749606
    Change-Id: I42e968a36611fbcc355dba8a6aa4a3e0ec0ad787
    Signed-off-by: Mostafa Saleh <smostafa@google.com>

--- arch/arm64/include/asm/kvm_pkvm_module.h
+++ arch/arm64/include/asm/kvm_pkvm_module.h
@@ -140,7 +140,9 @@ enum pkvm_psci_notification {
  * @__list_add_valid_or_report: Needed if the code uses linked lists.
  * @__list_del_entry_valid_or_report:
 				Needed if the code uses linked lists.
-* @iommu_iotlb_gather_add_page:	Add a page to the iotlb_gather druing unmap for the IOMMU.
+ * @iommu_iotlb_gather_add_page: Add a page to the iotlb_gather druing unmap for the IOMMU.
+ * @iommu_donate_pages_atomic:	Allocate memory from IOMMU identity pool.
+ * @iommu_reclaim_pages_atomic:	Reclaim memory from iommu_donate_pages_atomic()
  */
 struct pkvm_module_ops {
 	int (*create_private_mapping)(phys_addr_t phys, size_t size,
@@ -199,6 +201,8 @@ struct pkvm_module_ops {
 	int (*register_hyp_event_ids)(unsigned long start, unsigned long end);
 	void* (*tracing_reserve_entry)(unsigned long length);
 	void (*tracing_commit_entry)(void);
+	void * (*iommu_donate_pages_atomic)(u8 order);
+	void (*iommu_reclaim_pages_atomic)(void *p, u8 order);
 };
 
 int __pkvm_load_el2_module(struct module *this, unsigned long *token);
