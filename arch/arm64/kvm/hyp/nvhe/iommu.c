BayLibre/android_kernel_common

ANDROID: KVM: arm64: iommu: Create parent/child relation
In preparation for adding new IOMMU devices that act as suppliers to
others, add the notion of a parent IOMMU device. Such device must be
registered after its parent and the driver of the parent device must
validate the addition.

The relation has no generic implications, it is up to drivers to make
use of it.

Bug: 190463801
Signed-off-by: David Brazdil <dbrazdil@google.com>
Change-Id: I4ee3675e5529bb73ad4546fa32380f237f054177

arch/arm64/kvm/hyp/nvhe/iommu.c
@@ -285,6 +285,7 @@ out:
 int __pkvm_iommu_register(unsigned long dev_id,
 			  enum pkvm_iommu_driver_id drv_id,
 			  phys_addr_t dev_pa, size_t dev_size,
+			  unsigned long parent_id,
 			  void *kern_mem_va, size_t mem_size)
 {
 	struct pkvm_iommu *dev = NULL;
@@ -337,6 +338,7 @@ int __pkvm_iommu_register(unsigned long dev_id,
 
 	/* Populate the new device entry. */
 	*dev = (struct pkvm_iommu){
+		.children = LIST_HEAD_INIT(dev->children),
 		.id = dev_id,
 		.ops = drv->ops,
 		.pa = dev_pa,
@@ -348,6 +350,20 @@ int __pkvm_iommu_register(unsigned long dev_id,
 	if (!validate_against_existing_iommus(dev)) {
 		ret = -EBUSY;
 		goto out;
+	}
+
+	if (parent_id) {
+		dev->parent = find_iommu_by_id(parent_id);
+		if (!dev->parent) {
+			ret = -EINVAL;
+			goto out;
+		}
+
+		if (dev->parent->ops->validate) {
+			ret = dev->parent->ops->validate_child(dev->parent, dev);
+			if (ret)
+				goto out;
+		}
 	}
 
 	if (dev->ops->validate) {
@@ -363,6 +379,8 @@ int __pkvm_iommu_register(unsigned long dev_id,
 
 	/* Register device and prevent host from mapping the MMIO range. */
 	list_add_tail(&dev->list, &iommu_list);
+	if (dev->parent)
+		list_add_tail(&dev->siblings, &dev->parent->children);
 
 out:
 	host_unlock_component();

vrosendahl/kernel-nvidia-jetson

arch/arm64/kvm/hyp/nvhe/iommu.c
@@ -289,6 +289,7 @@ out:
 int __pkvm_iommu_register(unsigned long dev_id,
 			  enum pkvm_iommu_driver_id drv_id,
 			  phys_addr_t dev_pa, size_t dev_size,
+			  unsigned long parent_id,
 			  void *kern_mem_va, size_t mem_size)
 {
 	struct pkvm_iommu *dev = NULL;
@@ -333,6 +334,7 @@ int __pkvm_iommu_register(unsigned long dev_id,
 
 	/* Populate the new device entry. */
 	*dev = (struct pkvm_iommu){
+		.children = LIST_HEAD_INIT(dev->children),
 		.id = dev_id,
 		.ops = drv->ops,
 		.pa = dev_pa,
@@ -342,6 +344,20 @@ int __pkvm_iommu_register(unsigned long dev_id,
 	if (!validate_against_existing_iommus(dev)) {
 		ret = -EBUSY;
 		goto out;
+	}
+
+	if (parent_id) {
+		dev->parent = find_iommu_by_id(parent_id);
+		if (!dev->parent) {
+			ret = -EINVAL;
+			goto out;
+		}
+
+		if (dev->parent->ops->validate_child) {
+			ret = dev->parent->ops->validate_child(dev->parent, dev);
+			if (ret)
+				goto out;
+		}
 	}
 
 	if (dev->ops->validate) {
@@ -369,6 +385,8 @@ int __pkvm_iommu_register(unsigned long dev_id,
 
 	/* Register device and prevent host from mapping the MMIO range. */
 	list_add_tail(&dev->list, &iommu_list);
+	if (dev->parent)
+		list_add_tail(&dev->siblings, &dev->parent->children);
 
 out:
 	if (ret)