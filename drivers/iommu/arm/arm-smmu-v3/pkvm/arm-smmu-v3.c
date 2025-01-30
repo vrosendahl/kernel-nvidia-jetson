BayLibre/android_kernel_common

ANDROID: iommu/arm-smmu-v3-kvm: Support EL2 compiling as a module
Support compiling the EL2 driver as a module this includes:
- Using module ops at EL2 instead of direct function calls.
- Add module init function smmu_init_hyp_module.
- Add Kbuild file for EL2 module.

Built-in build is still supported.

Bug: 277989609
Bug: 278749606
Change-Id: I722271e896e1556a2a0cbcac0b2b0b5c20748039
Signed-off-by: Mostafa Saleh <smostafa@google.com>

drivers/iommu/arm/arm-smmu-v3/pkvm/arm-smmu-v3.c
@@ -14,6 +14,31 @@
 #include <nvhe/pkvm.h>
 #include <nvhe/trap_handler.h>
 
+#include "arm-smmu-v3-module.h"
+
+#ifdef MODULE
+void *memset(void *dst, int c, size_t count)
+{
+	return CALL_FROM_OPS(memset, dst, c, count);
+}
+
+#ifdef CONFIG_LIST_HARDENED
+bool __list_add_valid_or_report(struct list_head *new,
+				struct list_head *prev,
+				struct list_head *next)
+{
+	return CALL_FROM_OPS(__list_add_valid_or_report, new, prev, next);
+}
+
+bool __list_del_entry_valid_or_report(struct list_head *entry)
+{
+	return CALL_FROM_OPS(__list_del_entry_valid_or_report, entry);
+}
+#endif
+
+const struct pkvm_module_ops		*mod_ops;
+#endif
+
 #define ARM_SMMU_POLL_TIMEOUT_US	1000000 /* 1s! */
 
 size_t __ro_after_init kvm_hyp_arm_smmu_v3_count;
@@ -175,7 +200,6 @@ static int smmu_sync_cmd(struct hyp_arm_smmu_v3_device *smmu)
 	if (ret)
 		return ret;
 
-	hyp_assert_lock_held(&smmu->iommu.lock);
 	return smmu_wait_event(smmu, smmu_cmdq_empty(smmu));
 }
 
@@ -904,6 +928,17 @@ int smmu_resume(struct kvm_hyp_iommu *iommu)
 		return smmu_reset_device(smmu);
 	return 0;
 }
+
+#ifdef MODULE
+int smmu_init_hyp_module(const struct pkvm_module_ops *ops)
+{
+	if (!ops)
+		return -EINVAL;
+
+	mod_ops = ops;
+	return 0;
+}
+#endif
 
 struct kvm_iommu_ops smmu_ops = {
 	.init				= smmu_init,

vrosendahl/kernel-nvidia-jetson

drivers/iommu/arm/arm-smmu-v3/pkvm/arm-smmu-v3.c
@@ -16,6 +16,32 @@
 #include <nvhe/pkvm.h>
 #include <nvhe/trap_handler.h>
 
+
+#include "arm-smmu-v3-module.h"
+
+#ifdef MODULE
+void *memset(void *dst, int c, size_t count)
+{
+	return CALL_FROM_OPS(memset, dst, c, count);
+}
+
+#ifdef CONFIG_LIST_HARDENED
+bool __list_add_valid_or_report(struct list_head *new,
+				struct list_head *prev,
+				struct list_head *next)
+{
+	return CALL_FROM_OPS(list_add_valid_or_report, new, prev, next);
+}
+
+bool __list_del_entry_valid_or_report(struct list_head *entry)
+{
+	return CALL_FROM_OPS(list_del_entry_valid_or_report, entry);
+}
+#endif
+
+const struct pkvm_module_ops		*mod_ops;
+#endif
+
 #define ARM_SMMU_POLL_TIMEOUT_US	100000 /* 100ms arbitrary timeout */
 
 size_t __ro_after_init kvm_hyp_arm_smmu_v3_count;
@@ -789,6 +815,17 @@ int smmu_resume(struct kvm_hyp_iommu *iommu)
 		return smmu_reset_device(smmu);
 	return 0;
 }
+
+#ifdef MODULE
+int smmu_init_hyp_module(const struct pkvm_module_ops *ops)
+{
+	if (!ops)
+		return -EINVAL;
+
+	mod_ops = ops;
+	return 0;
+}
+#endif
 
 struct kvm_iommu_ops smmu_ops = {
 	.init				= smmu_init,