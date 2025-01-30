maltesermailo/android_kernel_pantah

ANDROID: KVM: arm64: Allow handling illegal aborts from pKVM modules
Introduce a new handler allowing to notify pKVM modules when pKVM
detects an illegal access from the host.

Bug: 244543039
Bug: 245034629
Change-Id: I62133a8d967d91437e5216b307e449f8c83dfab6
Signed-off-by: Quentin Perret <qperret@google.com>

arch/arm64/include/asm/kvm_pkvm_module.h
@@ -21,6 +21,7 @@ struct pkvm_module_ops {
 	int (*register_host_perm_fault_handler)(int (*cb)(struct kvm_cpu_context *ctxt, u64 esr, u64 addr));
 	int (*protect_host_page)(u64 pfn, enum kvm_pgtable_prot prot);
 	int (*register_host_smc_handler)(bool (*cb)(struct kvm_cpu_context *));
+	int (*register_illegal_abt_notifier)(void (*cb)(struct kvm_cpu_context *));
 };
 
 struct pkvm_module_section {

vrosendahl/kernel-nvidia-jetson

Bug: 278749606

arch/arm64/include/asm/kvm_pkvm_module.h
@@ -23,6 +23,7 @@ struct pkvm_module_ops {
 	int (*register_host_perm_fault_handler)(int (*cb)(struct user_pt_regs *regs, u64 esr, u64 addr));
 	int (*protect_host_page)(u64 pfn, enum kvm_pgtable_prot prot);
 	int (*register_host_smc_handler)(bool (*cb)(struct user_pt_regs *));
+	int (*register_illegal_abt_notifier)(void (*cb)(struct user_pt_regs *));
 };
 
 int __pkvm_load_el2_module(struct module *this, unsigned long *token);