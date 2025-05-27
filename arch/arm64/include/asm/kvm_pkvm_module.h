maltesermailo/android_kernel_pantah

ANDROID: arm64: kvm: Add new module functions used by s2mpu.
Add functions needed by S2MPU el2 module:
 -pkvm_host_donate_hyp/pkvm_hyp_donate_host: get and return SMPT from
  host kernel.
 -memcpy/memset: common functions used by s2mpu module.
 -hyp_pa: convert hyp address to PA, to get DMA address of SMPT.
 -kern_hyp_va: convert kernel address to hyp VA, used by the
  module to get the VA of SMPTs allocated from the kernel.

Change-Id: Idceccf0ac38444e7f267f50c476b99f01a613372
Signed-off-by: Mostafa Saleh <smostafa@google.com>

arch/arm64/include/asm/kvm_pkvm_module.h
@@ -30,6 +30,12 @@ struct pkvm_module_ops {
 	int (*register_default_trap_handler)(bool (*cb)(struct kvm_cpu_context *));
 	int (*register_illegal_abt_notifier)(void (*cb)(struct kvm_cpu_context *));
 	int (*register_psci_notifier)(void (*cb)(enum pkvm_psci_notification, struct kvm_cpu_context *));
+	int (*pkvm_host_donate_hyp)(u64 pfn, u64 nr_pages);
+	int (*pkvm_hyp_donate_host)(u64 pfn, u64 nr_pages);
+	void* (*memcpy)(void *to, const void *from, size_t count);
+	void* (*memset)(void *dst, int c, size_t count);
+	phys_addr_t (*hyp_pa)(void *x);
+	unsigned long (*kern_hyp_va)(unsigned long x);
 };
 
 struct pkvm_module_section {

arch/arm64/kvm/hyp/nvhe/modules.c
@@ -67,6 +67,12 @@ const struct pkvm_module_ops module_ops = {
 	.register_default_trap_handler = __pkvm_register_default_trap_handler,
 	.register_illegal_abt_notifier = __pkvm_register_illegal_abt_notifier,
 	.register_psci_notifier = __pkvm_register_psci_notifier,
+	.pkvm_host_donate_hyp = __pkvm_host_donate_hyp,
+	.pkvm_hyp_donate_host = __pkvm_hyp_donate_host,
+	.memcpy = memcpy,
+	.memset = memset,
+	.hyp_pa = hyp_virt_to_phys,
+	.kern_hyp_va = __kern_hyp_va,
 };
 
 int __pkvm_init_module(void *module_init)

vrosendahl/kernel-nvidia-jetson

Bug: 278749606
Bug: 261857213
Signed-off-by: Quentin Perret <qperret@google.com>

arch/arm64/include/asm/kvm_pkvm_module.h
@@ -35,6 +35,12 @@ struct pkvm_module_ops {
 	int (*register_illegal_abt_notifier)(void (*cb)(struct user_pt_regs *));
 	int (*register_psci_notifier)(void (*cb)(enum pkvm_psci_notification, struct user_pt_regs *));
 	int (*register_hyp_panic_notifier)(void (*cb)(struct user_pt_regs *));
+	int (*host_donate_hyp)(u64 pfn, u64 nr_pages);
+	int (*hyp_donate_host)(u64 pfn, u64 nr_pages);
+	void* (*memcpy)(void *to, const void *from, size_t count);
+	void* (*memset)(void *dst, int c, size_t count);
+	phys_addr_t (*hyp_pa)(void *x);
+	unsigned long (*kern_hyp_va)(unsigned long x);
 };
 
 int __pkvm_load_el2_module(struct module *this, unsigned long *token);

arch/arm64/kvm/hyp/nvhe/modules.c
@@ -108,6 +108,12 @@ const struct pkvm_module_ops module_ops = {
 	.register_illegal_abt_notifier = __pkvm_register_illegal_abt_notifier,
 	.register_psci_notifier = __pkvm_register_psci_notifier,
 	.register_hyp_panic_notifier = __pkvm_register_hyp_panic_notifier,
+	.host_donate_hyp = __pkvm_host_donate_hyp,
+	.hyp_donate_host = __pkvm_hyp_donate_host,
+	.memcpy = memcpy,
+	.memset = memset,
+	.hyp_pa = hyp_virt_to_phys,
+	.kern_hyp_va = __kern_hyp_va,
 };
 
 int __pkvm_init_module(void *module_init)