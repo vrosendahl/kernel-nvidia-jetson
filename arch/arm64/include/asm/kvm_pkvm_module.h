maltesermailo/android_kernel_pantah

ANDROID: KVM: arm64: Notify pKVM modules of PSCI events
Introduce a notifier allowing a pKVM module to be notified for major
PSCI events: {CPU,SYSTEM}_SUSPEND, as well as on the resume path.

Bug: 244543039
Bug: 245034629
Change-Id: Ia82923445214925fc77e321457c8eab31f9d42e8
Signed-off-by: Quentin Perret <qperret@google.com>

arch/arm64/include/asm/kvm_pkvm_module.h
@@ -8,6 +8,12 @@
 
 typedef void (*dyn_hcall_t)(struct kvm_cpu_context *);
 
+enum pkvm_psci_notification {
+	PKVM_PSCI_CPU_SUSPEND,
+	PKVM_PSCI_SYSTEM_SUSPEND,
+	PKVM_PSCI_RESUME,
+};
+
 struct pkvm_module_ops {
 	int (*create_private_mapping)(phys_addr_t phys, size_t size,
 				      enum kvm_pgtable_prot prot,
@@ -22,6 +28,7 @@ struct pkvm_module_ops {
 	int (*protect_host_page)(u64 pfn, enum kvm_pgtable_prot prot);
 	int (*register_host_smc_handler)(bool (*cb)(struct kvm_cpu_context *));
 	int (*register_illegal_abt_notifier)(void (*cb)(struct kvm_cpu_context *));
+	int (*register_psci_notifier)(void (*cb)(enum pkvm_psci_notification, struct kvm_cpu_context *));
 };
 
 struct pkvm_module_section {

arch/arm64/kvm/hyp/include/nvhe/modules.h
@@ -6,6 +6,9 @@
 int __pkvm_register_host_smc_handler(bool (*cb)(struct kvm_cpu_context *));
 int __pkvm_register_illegal_abt_notifier(void (*cb)(struct kvm_cpu_context *));
 
+enum pkvm_psci_notification;
+int __pkvm_register_psci_notifier(void (*cb)(enum pkvm_psci_notification, struct kvm_cpu_context *));
+
 #ifdef CONFIG_MODULES
 int __pkvm_init_module(void *module_init);
 int __pkvm_register_hcall(unsigned long hfn_hyp_va);

vrosendahl/kernel-nvidia-jetson

Bug: 278749606

arch/arm64/include/asm/kvm_pkvm_module.h
@@ -9,6 +9,12 @@
 typedef void (*dyn_hcall_t)(struct user_pt_regs *);
 
 #ifdef CONFIG_MODULES
+enum pkvm_psci_notification {
+	PKVM_PSCI_CPU_SUSPEND,
+	PKVM_PSCI_SYSTEM_SUSPEND,
+	PKVM_PSCI_CPU_ENTRY,
+};
+
 struct pkvm_module_ops {
 	int (*create_private_mapping)(phys_addr_t phys, size_t size,
 				      enum kvm_pgtable_prot prot,
@@ -24,6 +30,7 @@ struct pkvm_module_ops {
 	int (*protect_host_page)(u64 pfn, enum kvm_pgtable_prot prot);
 	int (*register_host_smc_handler)(bool (*cb)(struct user_pt_regs *));
 	int (*register_illegal_abt_notifier)(void (*cb)(struct user_pt_regs *));
+	int (*register_psci_notifier)(void (*cb)(enum pkvm_psci_notification, struct user_pt_regs *));
 };
 
 int __pkvm_load_el2_module(struct module *this, unsigned long *token);

arch/arm64/kvm/hyp/include/nvhe/modules.h
@@ -6,6 +6,9 @@
 int __pkvm_register_host_smc_handler(bool (*cb)(struct user_pt_regs *));
 int __pkvm_register_illegal_abt_notifier(void (*cb)(struct user_pt_regs *));
 
+enum pkvm_psci_notification;
+int __pkvm_register_psci_notifier(void (*cb)(enum pkvm_psci_notification, struct user_pt_regs *));
+
 #ifdef CONFIG_MODULES
 int __pkvm_init_module(void *module_init);
 int __pkvm_register_hcall(unsigned long hfn_hyp_va);